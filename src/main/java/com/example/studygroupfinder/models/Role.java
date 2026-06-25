97fb948cdf9afea2e0a7e5d187e191528ba59fdf
Keerthika N <nkeerthika09@gmail.com>
2026-06-25 10:50:10 +0530

    Role schema defined

--- src/main/java/com/example/studygroupfinder/models/Role.java
+++ src/main/java/com/example/studygroupfinder/models/Role.java
@@ -1,4 +1,24 @@
 package com.example.studygroupfinder.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int roleId;
+    @NotBlank  
+    private String role_name;
 }
